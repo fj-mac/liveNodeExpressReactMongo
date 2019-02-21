abd5661afd803b542af3c1aebe39ec59a9e3fdfd
John Alexis Guerra Gómez <john.guerra@gmail.com>
2019-02-21 17:37:35 -0500

    Serving react from express

--- front/src/App.js
+++ front/src/App.js
@@ -3,9 +3,24 @@ import React, {Component} from "react";
 import "./App.css";
 
 class App extends Component {
+
+  componentDidMount() {
+    fetch("/api/getMessages")
+      .then( res => console.log("got data?" ,res));
+  }
+
+
   render() {
     console.log("Rendering");
-    return (<div className="App">HOla John!!!!!!!</div>);
+    return (
+      <div className="App">
+        <h1>Comments!</h1>
+        <div id="comments"></div>
+        <form action="/api/createMessage">
+          <input type="text" id="comment"/>
+        </form>
+        <div>Made by John with <span role="img">♥️</span></div>
+      </div>);
   }
 }
 
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.post('/createMessage', function(req, res, next) {
+  res.send("Hola");
 });
+
+router.get('/getMessages', function(req, res, next) {
+  console.log("getMessages!!!!");
+  res.send([
+    {text:"John"}
+  ]);
+});
+
 
 module.exports = router;
